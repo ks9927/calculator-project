6da9916b7e4a29f52ea323deb5809400bd94e599
KS <180429671+ks9927@users.noreply.github.com>
2025-04-02 19:18:17 -0400

    Added operate function

--- math.js
+++ math.js
@@ -10,3 +10,19 @@ const divide = (g, h) => g / h;
 let firstNum;
 let operator;
 let secondNum;
+
+//operate function
+function operate(num1, oper, num2) {
+if (oper === '+') {
+    return add(num1, num2);
+}
+else if (oper === '-') {
+    return subtract(num1, num2);
+}
+else if (oper === '*') {
+    return multiply(num1, num2);
+}
+else if (oper === '/') {
+    return divide(num1, num2);
+}
+}
