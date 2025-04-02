d02e9d39465a309a7f45a0fb51b91534aa736f2a
KS <180429671+ks9927@users.noreply.github.com>
2025-04-02 19:10:01 -0400

    Add variables to update the display

--- math.js
+++ math.js
@@ -5,3 +5,8 @@ const subtract = (c, d) => c - d;
 const multiply = (e, f) => e * f;
 
 const divide = (g, h) => g / h;
+
+//variables to update the display
+let firstNum;
+let operator;
+let secondNum;
